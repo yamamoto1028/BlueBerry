0b936647eafa8626bbf140193c8690d16b9df02c
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-05 14:43:13 +0900

    コンソールで数字の判定処理機能

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-const message: string = "Hello, world!";
-console.log(message);
+import { createInterface } from "readline";
+
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`数値を入力してください:`, (line) => {
+  //文字列が入力されるとここが実行される
+  const num = Number(line);
+  if (0 <= num && num < 100) {
+    console.log(`${num}は0以上で100未満です`);
+  } else if (Number.isNaN(num)) {
+    console.log(`半角数字で入力してください`);
+  } else {
+    console.log(`${num}は対象範囲外の値です`);
+  }
+  rl.close();
+});
