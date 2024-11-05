38d1c6e4efef290422cc17866157ee83e2b059f8
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-05 15:00:06 +0900

    論理演算子による入力値判定

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,15 @@ const rl = createInterface({
 rl.question(`数値を入力してください:`, (line) => {
   //文字列が入力されるとここが実行される
   const num = Number(line);
-  if (0 <= num && num < 100) {
-    console.log(`${num}は0以上で100未満です`);
-  } else if (Number.isNaN(num)) {
-    console.log(`半角数字で入力してください`);
+  if (!Number.isNaN(num)) {
+    if (num >= 0 && num < 100) {
+      console.log(`${num}は0以上100以下です`);
+    } else {
+      console.log(`${num}は対象範囲外の値です`);
+    }
   } else {
-    console.log(`${num}は対象範囲外の値です`);
+    console.log(`半角数字で入力してください`);
   }
+
   rl.close();
 });
