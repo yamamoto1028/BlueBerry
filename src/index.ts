632d41a98e61e53b634e05aaad6bb360a05df7ce
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-05 15:51:12 +0900

    入力された名前をコンソールに表示。NULL、undefinedの場合ゲストを表示

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,10 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-rl.question(`数値を入力してください:`, (line) => {
+rl.question(`名前を入力してください:`, (name: string) => {
   //文字列が入力されるとここが実行される
-  const num = Number(line);
-  if (!Number.isNaN(num)) {
-    if (num >= 0 && num < 100) {
-      console.log(`${num}は0以上100以下です`);
-    } else {
-      console.log(`${num}は対象範囲外の値です`);
-    }
-  } else {
-    console.log(`半角数字で入力してください`);
-  }
+  const displayName = name || "ゲスト";
+  console.log(`こんにちは${displayName}さん`);
 
   rl.close();
 });
