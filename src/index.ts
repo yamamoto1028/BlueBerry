c71531cdb70feaf9cb53094de29c675689136538
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-06 15:41:04 +0900

    ⚡️ FizzBuzz_1行にまとめて出力する方法

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,17 @@
+let result = "";
+
 for (let i = 1; i <= 100; i++) {
+  if (i > 1) {
+    result += " "; //文字間に空白を追加する
+  }
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    result += "FizzBuzz";
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    result += "Fizz";
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    result += "Buzz";
   } else {
-    console.log(i);
+    result += String(i);
   }
 }
+console.log(result);
