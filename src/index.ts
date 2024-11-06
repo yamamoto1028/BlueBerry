41eb4df741fb0b876bbe02c542e11c205416e625
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-06 13:30:48 +0900

    while文

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,15 @@ const rl = createInterface({
   output: process.stdout,
 });
 
-rl.question(`名前を入力してください:`, (name) => {
+rl.question(`数値を入力してください:`, (name) => {
   //文字列が入力されるとここが実行される
-  switch (name) {
-    case "greet":
-      console.log("こんにちは");
-      break;
-    case "cat":
-      console.log("あなたは猫派？");
-      console.log("私は犬派です");
-      break;
-    default:
-      console.log(`${name}を認識できませんでした`);
+  let sum = 0;
+  let i = 1;
+  while (i <= 100) {
+    sum += i;
+    i++;
   }
+  console.log(sum);
 
   rl.close();
 });
