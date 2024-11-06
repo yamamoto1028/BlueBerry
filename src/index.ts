781158d2142d2f195c5bfc8f6dbe23afbeafa3ff
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-06 15:21:19 +0900

    FizzBuzz初鑑

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-let i = 0;
-while (i <= 100) {
-  i++;
-  if (i % 2 === 1) {
-    //iが奇数の場合、次のwhileループに移動する(logに出さない)
-    continue;
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
-  console.log(i);
 }
