870172219b4770d1768b62b57dc65f5d9e2414fe
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-08 10:23:26 +0900

    ✨ オブジェクトリテラル_スプレッド構文

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
-import { createInterface } from "readline";
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const messages = {
-  good: "0以上の数値が入力されました",
-  bad: "負の数値を入力しないでください！",
+const obj1 = {
+  bar: 456,
+  baz: 789,
 };
-
-rl.question("数値を入力してください", (line) => {
-  const num = Number(line);
-  if (typeof num === "number" && !isNaN(num)) {
-    console.log(messages[num >= 0 ? "good" : "bad"]);
-  } else {
-    console.log("半角数字で入力してください");
-  }
-  rl.close();
-});
+const obj2 = {
+  foo: 123,
+  ...obj1, //スプレッド構文_obj1のプロパティをコピーできる
+};
+console.log(obj2);
