269953d135e16a3e374de39b99bc4f6d1016ffaf
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-08 14:13:05 +0900

    ✨ インデックスシグネチャによるType宣言

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-interface FooBarObj {
-  foo: number;
-  bar: string;
-}
-
-const obj: FooBarObj = {
-  foo: 0,
-  bar: "string",
+type MsgType = {
+  [key: string]: string; //インデックスシグネチャ「どんな名前のプロパティも受け入れる」
 };
-console.log(obj); //今はあまり使われない。type文でOK。
+const messages: MsgType = {
+  msg1: "2の倍数です",
+  msg2: "5の倍数です",
+  msg3: "10の倍数です",
+};
+messages.msg4 = "3の倍数です";
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 === 0 && i % 5 === 0) {
+    console.log(messages.msg3);
+  } else if (i % 2 === 0) {
+    console.log(messages.msg1);
+  } else if (i % 5 === 0) {
+    console.log(messages.msg2);
+  } else {
+    console.log(i);
+  }
+}
