dd3367c857a759529dc0fc8112afe617f3f80cb2
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-08 15:44:47 +0900

    プロパティを読み取り専用で宣言

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 type MyObj = {
-  foo: boolean;
-  bar: boolean;
-  baz?: number;
+  readonly foo: number;
+};
+const obj: MyObj = {
+  foo: 123,
 };
 
-const obj: MyObj = { foo: true, bar: true };
-const obj2: MyObj = { foo: false, bar: true, baz: 1234 };
-
-console.log(obj2.baz * 1000); //これはコンパイルエラーundefinedの可能性がある値は計算できない
-if (obj2.baz !== undefined) {
-  //if文にてundefinedの可能性を除外すれば計算可能になる
-  console.log(obj2.baz * 1000);
-}
+obj.foo = 333;
+console.log(obj.foo);
