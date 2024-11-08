cd6c386966b4fd0cb3adf06851c4d117e546a6d8
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-08 17:26:24 +0900

    余剰プロパティに対する型エラーについて

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,19 @@
-type Animal = {
-  age: number;
-};
-type Human = {
-  age: number;
+type User = {
   name: string;
-};
-
-type AnimalFamily = {
-  familyName: string;
-  mother: Animal;
-  father: Animal;
-  child: Animal;
   age: number;
 };
-type HumanFamily = {
-  middleName: string;
-  mother: Human;
-  father: Human;
-  child: Human;
-  age: number;
-  firstName: string;
+const obj = {
+  name: "John",
+  age: 20,
+  telNumber: 1234567890,
 };
 
-const foo: HumanFamily = {
-  middleName: "foo",
-  mother: { age: 27, name: "Hanako" },
-  father: { age: 30, name: "Taro" },
-  child: { age: 4, name: "Kodomo" },
-  age: 27,
-  firstName: "Yamada",
-};
-console.log(
-  `この家の父は${foo.firstName}・${foo.middleName}・${foo.father.name}です。\n${foo.father.age}歳です。`
-);
+const u: User = obj; //別にオブジェクト用意し型付きの変数に代入するとエラーなし
+console.log(u);
+
+// --------------------------------------------------------------------------------------------------------------------
+// const u: User = {
+//   name: "John",
+//   age: 20,
+//   telNumber: 1234567890,
+// };//User型を指定したuにUser型にはないプロパティを指定するとコンパイルエラーとなる
