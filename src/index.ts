14745f4a0668e1eef4694569381bfd552ca6141c
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-12 13:54:17 +0900

    ✨ オプショナルな型引数

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
-type Hasname = {
+type Animal = {
   name: string;
 };
 
-type Family<Parent extends Hasname, Child extends Parent> = {
-  //ParentはHasnameの型定義を全て持ってないといけない、ChildはParentの型定義を全て持ってないといけない
+type Family<Parent = Animal, Child = Animal> = {
   father: Parent;
   mother: Parent;
   child: Child;
 };
-type Animal = {
-  name: string;
-};
-type Human = {
-  name: string;
-  age: number;
-};
 
-type S = Family<Animal, Human>; //HumanはAnimalの型定義を全て持ってないといけない
-type T = Family<Human, Animal>; //AnimalはHumanの型定義を全て持ってないといけない→持ってないのでコンパイルエラー
+type S = Family<string, string>; //通常の使い方
+type T = Family; //TはFamily<Animal, Animal>と同じ
+type U = Family<string>; //Uはfamily<string, Animal>と同じ
