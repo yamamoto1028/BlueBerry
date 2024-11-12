1bab0251128427274df60867c995db4037cfae7f
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-12 15:40:17 +0900

    ✨ 読み取り専用配列型

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
-const arr1: boolean[] = [false, true];
-
-const arr2: Array<{
-  name: string;
-  age: number;
-}> = [
-  { name: "山田さん", age: 15 },
-  { name: "田中さん", age: 16 },
-  { name: "鈴木さん", age: 17 },
-];
+const arr: readonly number[] = [1, 2, 3, 4];
+arr[1] = 500;
