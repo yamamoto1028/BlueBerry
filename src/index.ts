4103dea970603c0617ba9701b08bb9af621d83d4
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-12 14:15:37 +0900

    ⚡️ オプショナルな型引数の応用

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,41 @@
+type Human = {
+  name: string;
+  age: number;
+  flg: boolean;
+};
 type Animal = {
   name: string;
+  age: number;
+  flg: boolean;
+  kind: string;
 };
 
-type Family<Parent = Animal, Child = Animal> = {
+type Family<Parent = Human, Child = Human> = {
   father: Parent;
   mother: Parent;
   child: Child;
 };
 
-type S = Family<string, string>; //通常の使い方
-type T = Family; //TはFamily<Animal, Animal>と同じ
-type U = Family<string>; //Uはfamily<string, Animal>と同じ
+type S = Family<Human, Animal>; //通常の使い方
+type T = Family; //TはFamily<Human, Human>と同じ
+type U = Family<string>; //Uはfamily<string, Human>と同じ
+
+const familyS: S = {
+  father: { name: "ちち", age: 40, flg: true },
+  mother: { name: "はは", age: 38, flg: true },
+  child: { name: "いぬ", age: 5, flg: false, kind: "Dog" },
+};
+
+const familyT: T = {
+  father: { name: "ちち", age: 40, flg: true },
+  mother: { name: "はは", age: 38, flg: true },
+  child: { name: "いぬ", age: 5, flg: false },
+};
+
+const familyU: U = {
+  father: "ちち",
+  mother: "はは",
+  child: { name: "いぬ", age: 5, flg: false },
+};
+
+console.log(familyS, familyT, familyU);
