1ffa2ea6f758894b2510ea823fa7310d601a4805
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-15 12:34:28 +0900

    ✨オブジェクトの分割代入

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-const arr = [1, 10, 100];
-const num: number = arr[100]; //arr[100];→インデックスアクセス極力使用しない
-console.log(num); //number型の変数numにundefinedが代入されているという矛盾が発生。
+const obj = {
+  foo: 1,
+  bar: "2",
+};
+const { foo, bar } = obj; //objのプロパティ名と同じ変数名で定義して代入
+console.log(obj.foo);
+console.log(obj.bar);
+
+const obj1 = {
+  foo1: 1,
+  bar1: "2",
+  foobar: "3",
+};
+const { foo1, bar1: barVar, foobar: foobbar } = obj1; //obj1のbar1を変数名barVarに代入し宣言。foobarを変数名foobbarに代入し宣言
+console.log(obj1.foo1, barVar, foobbar);
