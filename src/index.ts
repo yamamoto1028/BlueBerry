2a133962e19165486c6c5aa97e2a2458fdddf706
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-15 18:00:33 +0900

    ✨ 配列の分割代入

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,38 @@
-const nested = {
-  num: 123,
-  obj: {
-    foo: "hello",
-    bar: "world",
-    mono: {
-      empitsu: "Good",
-      keshigomiu: "night", //いくつでもネストできる
+const arr = [1, 2, 4, 8, 16, 32];
+
+const [first, second, third] = arr;
+
+console.log(first, second, third); //1,2,4が表示される
+
+const obj = {
+  arr: [2, 4, 6, 8],
+  arr1: [1, 3, 5, 7, 9],
+  obj1: {
+    arr2: [1, 2, 3, 4],
+    obj2: {
+      name: "Taro",
+      age: 26,
+      from: "Hiroshima",
+      live: true,
     },
   },
 };
 const {
-  num,
-  obj: { bar: aa },
-  obj: { foo },
-  obj: {
-    mono: { empitsu: monosashi },
-  },
-  obj: {
-    mono: { keshigomiu: hasami },
-  },
-} = nested;
-console.log(num);
-console.log(foo);
-console.log(aa);
+  arr: [foo1, , foo3], //配列arrの0番目と2番目の要素を代入する。
 
-console.log(monosashi);
-console.log(hasami);
+  arr1: [foo, bar, baz],
+  obj1: {
+    arr2: [one, , three, four, five], //空白を用いて要素をスキップできる
+    obj2: { name: taroName, age: taroAge, live: taroLives }, //いろいろネストできる
+  },
+} = obj;
+console.log(foo, bar, baz); //1,3,5が表示
+console.log("foo1は" + foo1, "foo3は" + foo3);
+
+console.log(taroName, taroAge, taroLives);
+console.log(one, three);
+
+//タプル型もいける
+const tuple: [string, number] = ["Taro", 30];
+const [myName, age] = tuple;
+console.log(myName, age);
