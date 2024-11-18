e32f46600ce71a2f3d07f365fce8f8dd3bfae1d9
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-18 14:44:20 +0900

    ✨ 分割代入のデフォルト値

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,10 @@
-const arr = [1, 2, 4, 8, 16, 32];
+type obj = { foo?: number };
+const obj1: obj = {};
+const obj2: obj = { foo: -1234 };
 
-const [first, second, third] = arr;
+const { foo = 500 } = obj1; //obj1のfooを変数fooに代入。undefinedの場合は500を代入。
+console.log(foo); //500が出力
+console.log(obj1.foo); //undefined
 
-console.log(first, second, third); //1,2,4が表示される
-
-const obj = {
-  arr: [2, 4, 6, 8],
-  arr1: [1, 3, 5, 7, 9],
-  obj1: {
-    arr2: [1, 2, 3, 4],
-    obj2: {
-      name: "Taro",
-      age: 26,
-      from: "Hiroshima",
-      live: true,
-    },
-  },
-};
-const {
-  arr: [foo1, , foo3], //配列arrの0番目と2番目の要素を代入する。
-
-  arr1: [foo, bar, baz],
-  obj1: {
-    arr2: [one, , three, four, five], //空白を用いて要素をスキップできる
-    obj2: { name: taroName, age: taroAge, live: taroLives }, //いろいろネストできる
-  },
-} = obj;
-console.log(foo, bar, baz); //1,3,5が表示
-console.log("foo1は" + foo1, "foo3は" + foo3);
-
-console.log(taroName, taroAge, taroLives);
-console.log(one, three);
-
-//タプル型もいける
-const tuple: [string, number] = ["Taro", 30];
-const [myName, age] = tuple;
-console.log(myName, age);
+const { foo: bar = 500 } = obj2;
+console.log(bar); //-1234が出力
