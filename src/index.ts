3eacd99da3b139e74d6347034843ee7059004ff0
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-19 11:34:35 +0900

    ✨ Dateオブジェクト

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-const obj = {
-  foo: 123,
-  bar: true,
-  bazz: "string",
-};
-const { foo, ...restObj } = obj; //残りのプロパティを全て持つ新たなオブジェクトを代入。
-console.log(foo);
-console.log(restObj);
+const d = new Date();
+d.setFullYear(2030);
+console.log(d);
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const [a, b, c, ...restArray] = arr; //その位置以降のすべての要素を新たな配列にコピーする。
-console.log(a, b, c);
-console.log(restArray);
+const d1 = new Date("2020-02-03T15:00:00+09:00"); //ISO8601形式の場合、文字列として渡せばDateオブジェクトに変換できる。
+console.log(d1);
+
+const d2 = new Date("2020-02-03T15:00:00+09:00");
+const timeNum = d2.getTime(); //数値のよる表現（UNIX時間）1970/1/1 0:00:00からの経過時間（ミリ秒単位）を表したもの。
+console.log(timeNum);
+const d3 = new Date(timeNum); //数値からDateオブジェクトを得る
+console.log(d3);
+
+console.log(Date.now()); //現在時刻を数値で得るメソッド
+const d4 = new Date();
+console.log(d4);
