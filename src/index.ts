b27851ff38bb63e51c2e8b55dad1e211997f5381
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-19 13:15:43 +0900

    ✨ 正規表現オブジェクト_正規表現の基本

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,10 @@
-const d = new Date();
-d.setFullYear(2030);
-console.log(d);
+const r = /ab+c/; //正規表現オブジェクト
 
-const d1 = new Date("2020-02-03T15:00:00+09:00"); //ISO8601形式の場合、文字列として渡せばDateオブジェクトに変換できる。
-console.log(d1);
+console.log(r.test("abbbbc")); //testメソッド→文字列型を受け取り、文字列のどこか一部分が正規表現にマッチしているか真偽値を返す
+console.log(r.test("Hello, abc world!"));
+console.log(r.test("ABC"));
+console.log(r.test("こんにちは"));
 
-const d2 = new Date("2020-02-03T15:00:00+09:00");
-const timeNum = d2.getTime(); //数値のよる表現（UNIX時間）1970/1/1 0:00:00からの経過時間（ミリ秒単位）を表したもの。
-console.log(timeNum);
-const d3 = new Date(timeNum); //数値からDateオブジェクトを得る
-console.log(d3);
-
-console.log(Date.now()); //現在時刻を数値で得るメソッド
-const d4 = new Date();
-console.log(d4);
+const s = /^abc/; //文字列の先頭にあるabcにのみマッチする
+console.log(s.test("abcdefg"));
+console.log(s.test("aabcdefg"));
