f67252a596ee6c8d99d5b968f6dd074894206f5b
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-21 15:20:48 +0900

    ⚡️ 返り値がない関数を作る

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
-//function 関数名 (引数リスト):返り値の型{中身}
+//返り値がない関数を宣言するためには、返り値の型としてvoid型という特殊な型を使用する。
 
-console.log(range(3, 20)); //関数の巻き上げ(hosting)…関数宣言よりも先に呼び出してもOK
-function range(min: number, max: number): number[] {
-  const result = [];
-  for (let i = min; i <= max; i++) {
-    result.push(i);
+//（例）引数nを受け取って"Hello world"とn回表示するだけの返り値のない関数
+function helloWorldNTimes(n: number): void {
+  for (let i = 0; i < n; i++) {
+    console.log("Hello world");
   }
-  return result;
 }
-console.log(range(3, 10));
+
+helloWorldNTimes(5);
+
+function helloWorldNTimes2(n: number): void {
+  //返り値のない関数の返り値の型はvoidにする
+  if (n >= 100) {
+    console.log(`${n}回なんて無理です！`);
+    return; //返り値がある場合、returnと返り値の間に改行を入れてはいけない
+  }
+  for (let i = 1; i <= n; i++) {
+    console.log(`Hello world(${i}回目)`);
+  }
+}
+helloWorldNTimes2(5);
+helloWorldNTimes2(150);
