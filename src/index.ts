2878eebc1b5a92ef3103ce7a5d6f6bb30d0db3d0
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-21 16:02:34 +0900

    ⚡️ 関数式による関数作成

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,14 @@
-//返り値がない関数を宣言するためには、返り値の型としてvoid型という特殊な型を使用する。
+type Human = {
+  height: number;
+  weight: number;
+};
 
-//（例）引数nを受け取って"Hello world"とn回表示するだけの返り値のない関数
-function helloWorldNTimes(n: number): void {
-  for (let i = 0; i < n; i++) {
-    console.log("Hello world");
-  }
-}
+const calcBMI = function (human: Human): number {
+  return human.weight / human.height ** 2;
+};
 
-helloWorldNTimes(5);
-
-function helloWorldNTimes2(n: number): void {
-  //返り値のない関数の返り値の型はvoidにする
-  if (n >= 100) {
-    console.log(`${n}回なんて無理です！`);
-    return; //返り値がある場合、returnと返り値の間に改行を入れてはいけない
-  }
-  for (let i = 1; i <= n; i++) {
-    console.log(`Hello world(${i}回目)`);
-  }
-}
-helloWorldNTimes2(5);
-helloWorldNTimes2(150);
+const Taro: Human = {
+  height: 1.68,
+  weight: 80,
+};
+console.log(calcBMI(Taro));
