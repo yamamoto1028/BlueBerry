706a8207c21dbf915f86bc6d5fe5e8c0515a1898
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-21 16:18:36 +0900

    ⚡️ 関数式で関数を作る_引数の分割代入

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,11 @@ type Human = {
   weight: number;
 };
 
-const calcBMI = function (human: Human): number {
-  return human.weight / human.height ** 2;
+console.log(calcBMI(Taro)); //変数宣言よりも前に関数の呼び出しはできない…巻き上げ機能は使えない
+
+const calcBMI = function ({ height, weight }: Human): number {
+  //humanのところを分割代入でプロパティを直接書ける
+  return weight / height ** 2;
 };
 
 const Taro: Human = {
