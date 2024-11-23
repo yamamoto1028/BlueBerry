c65719be0ef375527a8a42dccdc42a59dccb918c
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-23 16:14:20 +0900

    ✨ 関数の型・関数型の記法

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,13 @@
-//コールバック関数について…引数として渡される関数のこと
-type User = {
-  name: string;
-  age: number;
-};
+//関数の型 関数型の記法
+//(引数: 引数の型): 返り値の型 => 中身
+//xRepeatにカーソル乗せると型の定義→(引数リスト) => 返り値の型
+const xRepeat = (num: number): string => "x".repeat(num); //repeatメソッドは文字列が持つメソッド
+console.log(xRepeat(2));
 
-//コールバック関数-------------------------------------------------------------------------------
-const getName = (u: User): string => {
-  //(u)に配列の各要素が順番に入る
-  console.log("u is", u);
-  return u.name; //各要素のnameプロパティの値を返す
-};
-const getAge = (u: User): number => {
-  return u.age;
-};
+//type文で型注釈をつけられる
+type F = (repeatNum: number) => string; //→(引数リスト) => 返り値の型
+const xRepeat2: F = (num: number) => "x".repeat(num);
 
-//Userの配列オブジェクト-------------------------------------------------------------------------
-const users: User[] = [
-  { name: "Taro", age: 26 },
-  { name: "John Smith", age: 15 },
-];
-
-//usersの各要素ひとつずつにgetName関数を実行-------------------------------------------------------
-const names = users.map(getName); //コールバック関数
-console.log(names);
-//変数getNameを経由しない書き方↓
-const names1 = users.map((u: User): string => u.name);
-console.log(names1);
-
-const ages = users.map(getAge); //コールバック関数
-console.log(ages);
-//変数getAgeを経由しない書き方↓
-const ages1 = users.map((u: User): number => u.age);
-console.log(ages1);
-
-//そのほかの配列が持つコールバックを生かしたメソッド---------------------------------------------------
-//20歳以上のユーザーだけの配列
-const adultUsers = users.filter((u: User) => u.age >= 20);
-//全てのユーザーが20以上ならTRUE
-const allAdult = users.every((u: User) => u.age >= 20);
-//60歳以上のユーザーが1人でもいればTRUE
-const seniorExists = users.some((u: User) => u.age >= 60);
-//名前がJohnで始まるユーザーを探して返す
-const john = users.find((u: User) => u.name.startsWith("John"));
-
-console.log(adultUsers);
-console.log(allAdult);
-console.log(seniorExists);
-console.log(john);
+//下記、型に合わない関数を変数に代入するのはコンパイルエラー
+type F2 = (arg: string, arg2: string) => boolean;
+const fun: F2 = (num1: string): boolean => console.log(num1);
