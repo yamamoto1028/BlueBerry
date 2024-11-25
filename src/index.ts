839ee27e2f8a57bb7e35d8cbfc723cb2c6430b87
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-25 14:06:12 +0900

    ⚡️ 返り値の型注釈は省略可能

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
-//関数の型 関数型の記法
-//(引数: 引数の型): 返り値の型 => 中身
-//xRepeatにカーソル乗せると型の定義→(引数リスト) => 返り値の型
-const xRepeat = (num: number): string => "x".repeat(num); //repeatメソッドは文字列が持つメソッド
-console.log(xRepeat(2));
+//返り値の型推論は省略可能
+//省略した場合は型推論によって決められる
+const xRepeat = (num: number) => "x".repeat(num); //返り値が型推論によってstringと判断された
 
-//type文で型注釈をつけられる
-type F = (repeatNum: number) => string; //→(引数リスト) => 返り値の型
-const xRepeat2: F = (num: number) => "x".repeat(num);
-
-//下記、型に合わない関数を変数に代入するのはコンパイルエラー
-type F2 = (arg: string, arg2: string) => boolean;
-const fun: F2 = (num1: string): boolean => console.log(num1);
+//返り値を返さない関数も型注釈をつけられる→返り値はvoid型になる
+const g = (num: number) => {
+  for (let i = 0; i < num; i++) {
+    console.log("Hello, World!");
+  }
+};
+g(3);
