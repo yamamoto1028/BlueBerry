bcd669348e9d2a801e2c643fcbc5dc9912a39e28
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-26 16:24:30 +0900

    ✨ 菅姿の部分型関係_返り値の型による部分型関係

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,21 @@
-//コールシグネチャによる関数型の表現
-//(引数リスト):返り値の型;
-type myFunc = {
-  isUsed?: boolean; //プロパティの定義
-  (arg: number): void; //コールシグネチャの定義
+type HasName = {
+  name: string;
 };
-const double: myFunc = (arg: number) => {
-  console.log(arg + 2);
+type HasNameAndAge = {
+  name: string;
+  age: number;
 };
-double.isUsed = true; //doubleはisUsedプロパティを持つ
-console.log(double.isUsed);
-double(2); //関数としても呼び出せる
 
-//普通の関数型もコールシグネチャを使って書ける
-type F = (arg: string) => number; //普通の関数型
-type G = { (arg: string): number }; //コールシグネチャ(普通の関数型でかけるならあんまりやらないけど)
+const fromAge = (age: number): HasNameAndAge => ({
+  name: "John Smith",
+  age,
+});
+const f: (age: number) => HasName = fromAge; //HasNameAndAge型のfromAgeをHasName型のfに代入できている（部分型：fromAgeはfの部分型）
+const obj: HasName = f(100);
+console.log(obj);
 
-//string型を引数で渡すとnumber型を返し、number型を引数で渡すとboolean型で返す→コールシグネチャの出番
-type SwapFunc = {
-  (arg: string): number;
-  (arg: number): boolean;
+//void型はどんな値を返す関数でも部分型として使える。
+const g = (name: string) => {
+  name;
 };
-const H: SwapFunc = (arg: string | number): number | boolean => {
-  //function H(arg: string): number;
-  // function H(arg: number): boolean;
-  // function H(arg: string | number): number | boolean{ ←オーバーロードをシグネチャを明示的に記述するとHのエラーは解消できる。
-  if (typeof arg === "string") {
-    return 1;
-  } else {
-    return false;
-  }
-};
-console.log(H("5"));
-console.log(H(5));
+const h: (name: string) => void = g;
