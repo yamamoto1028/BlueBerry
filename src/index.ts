6ab84f63e566ec5fd835d20150c49be3235409f3
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-27 11:16:55 +0900

    ✨ 変数スコープと関数

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,37 @@
-//型引数を持つ関数型
+//変数スコープと関数
+//変数のスコープとは 変数の有効範囲 のこと
 const repeat = function <T>(element: T, length: number): T[] {
+  //引数も関数のスコープに属する
+  const result: T[] = []; //変数resultはrepeatのスコープに属する
+  for (let i = 0; i < length; i++) {
+    result.push(element);
+  }
+  const result = []; //スコープ内で複数回同じ変数を宣言することはできない
+
+  return result;
+};
+console.log(result); //repeat関数の外にはresultは存在しない
+
+//内側のスコープから外側のスコープにはアクセスできる--------------------------------------------------
+const repeatLength = 5;
+const repeat1 = function <T>(element: T): T[] {
   const result: T[] = [];
-  for (let i = 0; i < length; i++) {
+  for (let i = 0; i < repeatLength; i++) {
+    //関数の外のrepeatLengthを参照している
     result.push(element);
   }
   return result;
 };
-
-type Func = <T>(arg: T, num: number) => T[];
-
-const repeat1: Func = function (element, length) {
-  const result = [];
-  for (let i = 0; i < length; i++) {
+console.log(repeat1("3"));
+//-------------------------------------------------------------------------------------------
+const repeatLength1 = 5; //モジュールスコープ
+const repeat2 = function <T>(element: T): T[] {
+  const repeatLength1 = 3; //関数スコープ…内側が優先して参照される
+  const result: T[] = [];
+  for (let i = 0; i < repeatLength1; i++) {
     result.push(element);
   }
   return result;
 };
-
-console.log(repeat1("3", 5));
-console.log(repeat1(3, 5));
-console.log(repeat("3", 5));
+console.log(repeat2("3"));
+console.log(repeatLength1); //関数の外側ならモジュールスコープが参照される
