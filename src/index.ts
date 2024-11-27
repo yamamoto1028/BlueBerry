a8f7770064f283ab5767e883236b4c8f35025f6c
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-27 15:04:51 +0900

    ✨力試し_関数の作成

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,44 @@
-//ブロックスコープと関数スコープ
-//ブロックスコープとは ブロック{…}の範囲に対して発生する
-
-//年齢20歳以上なら5歳サバを読んで返す関数
-function sabayomi(age: number) {
-  if (age >= 18 && age < 20) {
-    //ブロックスコープ
-    const lie = age + 2;
-    return lie;
-  } else if (age >= 20) {
-    const lie = age - 5; //ブロックスコープことなるスコープなら同じ変数名を使える
-    return lie;
+//力試し_簡単な関数を書いてみよう
+//FizzBazz
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
   }
-  console.log(lie); //lieが存在しないコンパイルエラー
-  return age;
 }
-// console.log(sabayomi(19));
-
-//for文の()内の変数はそのfor文の中でのみ使える
-function sum(arr: number[]): number {
-  let result = 0;
-  for (let i = 0; i < arr.length; i++) {
-    result += arr[i];
-    console.log(i); //iは参照可能
+//for文の中の処理を関数に抜き出す--------------------------------------------------------
+function getFizzBuzzString(i: number): string {
+  if (i % 3 === 0 && i % 5 === 0) {
+    return "FizzBuzz";
+  } else if (i % 3 === 0) {
+    return "Fizz";
+  } else if (i % 5 === 0) {
+    return "Buzz";
+  } else {
+    return String(i);
   }
-  console.log(result);
-  // console.log(i); //iは見つからない
-
-  return result;
 }
-const sumArr: number[] = [1, 2, 3, 4, 5, 6];
-sum(sumArr);
+
+for (let i = 1; i <= 100; i++) {
+  const message = getFizzBuzzString(i); //iの値がなんの倍数かによって返す文字列を変える
+  console.log(message);
+}
+
+//for-of文で繰り返し処理を書く----------------------------------------------------------
+function sequence(start: number, end: number): number[] {
+  let arr: number[] = []; //numberの配列型の空配列を用意。
+  for (let i = start; i <= end; i++) {
+    arr.push(i); //i~jまでの数値を配列に繰り返し追加。
+  }
+  return arr; //出来上がったarrを呼び出し元に返す
+}
+
+for (const i of sequence(1, 100)) {
+  const message = getFizzBuzzString(i);
+  console.log(message);
+}
