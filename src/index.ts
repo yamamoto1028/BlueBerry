80ebf2dadc314a69889a8c8a767687e71fbed069
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-28 12:10:11 +0900

    ⚡️ メソッドを宣言する

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,21 @@
-//プロパティを宣言する
-//プロパティ名:型=式(初期値);
-//
-
+//メソッドを宣言する
+//クラス宣言の中にメソッドの宣言も書ける
+//thisはそれぞれ作成されたインスタンスのことを指す。それぞれを独立して操作できる
 class User {
-  //プロパティ名:型=式(初期値);
   name: string = "";
   age: number = 0;
+  isAdult(): boolean {
+    return this.age >= 20;
+  }
+  setAge(newAge: number) {
+    this.age = newAge;
+  }
 }
-
 const taro = new User();
-console.log(taro.name); //""が表示
-console.log(taro.age); //0が表示
+const baby = new User();
+console.log(taro.isAdult()); //false
 
-//オプショナルなプロパティの例--------------------------------------------------------
-class User1 {
-  name?: string; //初期値を省略できる
-  age: number = 0;
-}
-const jiro = new User1();
-console.log(jiro.name); //undefinedが出力
-jiro.name = "Jiro";
-console.log(jiro.name);
-//--------------------------------------------------------------------------------
-
-//読み取り専用プロパティの例----------------------------------------------------------
-class User2 {
-  readonly name: string = "Saburo";
-  age: number = 0;
-}
-const saburo = new User2();
-console.log(saburo.name);
-saburo.name = "Hanako"; //読み取り専用には代入できないためコンパイルエラー
-
-//--------------------------------------------------------------------------------
+taro.setAge(26);
+console.log(taro.age); //26
+console.log(taro.isAdult()); //true
+console.log(baby.isAdult()); //false
