5d0149ac1be28db56dc44dffa020e1e327229bdd
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-28 17:00:07 +0900

    ⚡️ 3種類のアクセシビリティ修飾子

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,25 @@
-//静的プロパティ・静的メソッド→クラスそのものに属するプロパティ・メソッド
-//クラス宣言には静的プロパティ・静的メソッドのせんげんを含むことができる
-//宣言の前にstaticと付けることで宣言できる
+// 3種類のアクセシビリティ修飾子
+// クラス宣言内のプロパティ宣言・メソッド宣言には アクセシビリティ修飾子 を付加できる
+// public・protected・private
+
+// publicは何も書かないのと同じ
+// privateはそのクラスの中からだけアクセスできる
+// protectedは private ＋ そのクラスを継承した子クラス からもアクセスできる
+//  →実装例はクラスの継承で！
 
 class User {
-  static adminName: string = "Taro"; //Userクラス(オブジェクト)だけのプロパティ
-  static getAdminUser() {
-    return new User(User.adminName, 26);
-  } //Userクラス(オブジェクト)だけのメソッド
   name: string;
-  age: number;
+  private age: number;
+
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
-  isAdult(): boolean {
+  public isAdult(): boolean {
     return this.age >= 20;
   }
 }
-console.log(User.adminName); //"Taro" →→→→→ Userオブジェクト（クラス）へのプロパティアクセスはできる
-const admin = User.getAdminUser();
-console.log(admin.age); //26
-console.log(admin.isAdult()); //true
-
 const taro = new User("Taro", 26);
-console.log(taro.adminName); //コンパイルエラー →→→→→ adminNameはUserクラスのプロパティで、インスタンス(taro)のプロパティではないためアクセスできない
+console.log(taro.name); //Taro
+console.log(taro.isAdult()); //true
+console.log(taro.age); //コンパイルエラー プライベートのプロパティのためクラスの外からはアクセスできない
