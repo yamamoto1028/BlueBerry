8057827ee331af3d8aaffabd858ae2f3ad96da5e
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-29 11:42:00 +0900

    ⚡️ クラスの静的初期化ブロック(staticブロック)

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,58 @@
-// もう１つのプライベートプロパティ
-// #プロパティ名 という記載でprivateプロパティと同じ振る舞いをする
+// クラスの静的初期化ブロック(staticブロック)
+// クラス宣言の中に static{…}
+// staticブロックは文を"クラスの宣言の一部"として書くことができるので、
+// プライベートプロパティへのアクセスを通じた特権的な処理が可能になる。
 
-const User = class {
-  name: string;
+console.log("Hello");
+class C {
+  static {
+    console.log("Taro");
+  }
+}
+console.log("world");
+// Hello
+// Taro
+// world
+
+// インスタンスの#ageはsetAge()を通してしか変更できない。
+// 0~150の範囲外の値を設定することはできない
+class User {
   #age: number;
-
-  constructor(name: string, age: number) {
-    this.name = name;
+  constructor() {
+    this.#age = 0;
+  }
+  getAge() {
+    return this.#age;
+  }
+  setAge(age: number) {
+    if (age < 0 || age > 150) {
+      return;
+    }
     this.#age = age;
   }
-  public isAdult(): boolean {
-    return this.#age >= 20;
+}
+
+// 上記の制限を、staticブロックを使うことで強引に突破するユーザを作ることができる
+class User1 {
+  // ------------------------------------
+  static adminUser: User1;
+  static {
+    this.adminUser = new User1();
+    this.adminUser.#age = 9999;
   }
-};
-
-const taro = new User("Taro", 26);
-console.log(taro.name); //Taro
-console.log(taro.isAdult()); //true
-console.log(taro.#age); //プライベート識別子のため、コンパイルエラー
-
-//プライベート識別子はとりあえず # 使っとけばまちがいねえ！不都合あったらprivateを検討
+  // ------------------------------------
+  #age: number;
+  constructor() {
+    this.#age = 0;
+  }
+  getAge() {
+    return this.#age;
+  }
+  setAge(age: number) {
+    if (age < 0 || age > 150) {
+      return;
+    }
+    this.#age = age;
+  }
+}
+console.log(User1.adminUser.getAge()); //9999
