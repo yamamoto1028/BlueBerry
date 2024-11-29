2b30ef941c11f96bd501595f43fb5bc4d8c3fc11
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-29 12:12:30 +0900

    ⚡️ 型引数を持つクラス

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,23 @@
-// クラスの静的初期化ブロック(staticブロック)
-// クラス宣言の中に static{…}
-// staticブロックは文を"クラスの宣言の一部"として書くことができるので、
-// プライベートプロパティへのアクセスを通じた特権的な処理が可能になる。
+// 型引数を持つクラス
 
-console.log("Hello");
-class C {
-  static {
-    console.log("Taro");
-  }
-}
-console.log("world");
-// Hello
-// Taro
-// world
-
-// インスタンスの#ageはsetAge()を通してしか変更できない。
-// 0~150の範囲外の値を設定することはできない
-class User {
+class User<T> {
+  name: string;
   #age: number;
-  constructor() {
-    this.#age = 0;
-  }
-  getAge() {
-    return this.#age;
-  }
-  setAge(age: number) {
-    if (age < 0 || age > 150) {
-      return;
-    }
+  readonly data: T;
+
+  constructor(name: string, age: number, data: T) {
+    this.name = name;
     this.#age = age;
+    this.data = data;
+  }
+  public isAdult(): boolean {
+    return this.#age >= 20;
   }
 }
 
-// 上記の制限を、staticブロックを使うことで強引に突破するユーザを作ることができる
-class User1 {
-  // ------------------------------------
-  static adminUser: User1;
-  static {
-    this.adminUser = new User1();
-    this.adminUser.#age = 9999;
-  }
-  // ------------------------------------
-  #age: number;
-  constructor() {
-    this.#age = 0;
-  }
-  getAge() {
-    return this.#age;
-  }
-  setAge(age: number) {
-    if (age < 0 || age > 150) {
-      return;
-    }
-    this.#age = age;
-  }
-}
-console.log(User1.adminUser.getAge()); //9999
+// new クラス名 <型引数リスト>(引数リスト)
+const taro = new User<string>("Taro", 26, "追加データ"); //taroはUser<string>型
+const data = taro.data; //dataはstring型
+
+const john = new User("John", 15, { num: 123 }); //johnはUser<{num:number}>型
+const data2 = john.data; //data2は{ num: 123 }型
