ca561482027eb176e97364fe8da7932c06a930fe
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-12-02 21:38:25 +0900

    ⚡️ 例外処理と大域脱出

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,53 @@
-// 例外をキャッチするtry-catch文
-// 例外処理（ランタイムエラー）が発生してもプログラムが強制終了しないようにする機構
-// try {
-//   tryブロック（複数書ける）
-// }catch(err){
-//   catchブロック（複数書ける）
-// }
-// try文で例外が発生した場合にcatch文に移行する
-const throwError = () => {
-  const error = new Error("エラーが発生しました！");
+// 例外処理と大域脱出
+// try-catch文は強力な言語機能だが取り扱いには注意
+function somefunc() {
+  const error = new Error("エラーです！");
   throw error;
-};
+}
 
 try {
-  console.log("エラーを発生させます");
-  throwError(); //try文はここで終わり、catch文へ移行する
-  console.log("エラーを発生させました"); //実行されない
+  somefunc();
 } catch (err) {
-  console.log("エラーをキャッチしました");
-  console.log(err); //Errorオブジェクトの表示
+  //何も書かないなにもしない
 }
-console.log("おわり"); //例外が発生しても処理は止まらずここまで実行される
+// キャッチされたエラーの中身を見なかったり、それどころかエラーの処理を何もしないで次に進んだりするのは悪い例外処理の典型。
+
+// 失敗を表すthrow以外の選択肢としては失敗を表す値を返すというものがある
+// 例)エラーを投げる代わりにundefinedを返すようにできる
+
+function getAverage(nums: number[]) {
+  if (nums.length === 0) {
+    return undefined;
+  }
+  let sum: number = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    console.log(sum);
+  }
+  return sum / nums.length;
+}
+
+const result = getAverage([]);
+console.log(result);
+
+try {
+  throwError();
+  //tryブロックから脱出するのでここは表示されない
+  console.log("ここは表示されない");
+} catch (err) {
+  console.log(err);
+}
+function throwError() {
+  const error = new Error("エラーが発生");
+  throw error;
+  // 関数から脱出するのでここは表示されない
+  console.log("ここも表示されない");
+}
+
+// ランタイムエラーは明示的に書いたthrow文だけでなく、それ以外の原因でも発生する可能性がある。
+try {
+  const bigInt = BigInt("foobar"); //ここでエラー発生
+  console.log(bigInt); //ここは実行されない
+} catch (err1) {
+  console.log(err1);
+}
