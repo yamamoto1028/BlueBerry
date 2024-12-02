2756d654d4d16c5fa152d66d8ad49b9f0c2eecc7
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-12-02 10:35:19 +0900

    ⚡️ instanceof演算子と型の絞り込み

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,52 @@
-// newシグネチャによるインスタンス化可能性の表現
-// クラスオブジェクトをnewで呼び出してインスタンスを作る型の記法
-// new(引数リスト)=>インスタンスの型
+// instanceof演算子と型の絞り込み
+// 与えられたオブジェクトがあるクラスのインスタンスかどうかを判断する演算子
 class User {
   name: string = "";
   age: number = 0;
 }
-type MyUserConstructor = new () => User;
+const taro = new User();
+console.log(taro instanceof User); //taroはUserのインスタンスなのでtrue
+console.log({} instanceof User); //{}はUserのインスタンスではないのでfalse
 
-const MyUser: MyUserConstructor = User; //UserはMyUserConstructor型を持つ
-const u = new MyUser(); //MyUserはnewで使用可能
-console.log(u.name, u.age); //uはUser型をもつ
-
-//newシグネチャというコールシグネチャの亜種
-type MyUserConstructor1 = {
-  new (): User;
-  // newで呼び出せると同時に、それ自身もプロパティやメソッドを持つ
+const john: User = {
+  name: "John Smith",
+  age: 15,
 };
+console.log(john instanceof User); //johnはUserのインスタンスではないのでfalse
+// ------------------------------------------------------------------------------------------------------------
+// この演算子の特徴は型の絞り込みをサポートしている点------------------------------------------------------------------
+type HasAge = {
+  age: number;
+};
+class User1 {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+function getPrice(customer: HasAge) {
+  if (customer instanceof User1) {
+    if (customer.name === "Jiro") {
+      return 0;
+    }
+  }
+  return customer.age < 18 ? 1000 : 1800;
+}
+const customer1: HasAge = { age: 15 };
+const customer2: HasAge = { age: 40 };
+const jiro = new User1("Jiro", 26);
+
+console.log(getPrice(customer1)); //1000
+console.log(getPrice(customer2)); //1800
+console.log(getPrice(jiro)); //0
+
+// この書き方も可
+// if(customer instanceof User1 && customer.name === "Jiro"){
+//  return 0
+// }
+
+// TypeScriptではクラスを使わずにデータを表現することが多くあるので、instanceofのようなクラスに依存したロジックはあまり好ましくない
