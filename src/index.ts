6c5b7ab4da9a39b51f0d47a1ae655833ceaebeac
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-12-02 14:44:18 +0900

    ⚡️ implementsキーワードによるクラスの型チェック

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-// privateとprotectedの動作と使いどころ
-// protected → 子クラスからはアクセスできる
-// private(#) → 子クラスからもアクセスできない
-class User {
+// implementsキーワードによるクラスの型チェック
+// クラスを作成する際はimplementsキーワードによる追加の型チェックを利用可能
+// class クラス名 implements 型 {…}
+// そのクラスのインスタンスは与えられた型の部分型であるという宣言
+
+type HasName = {
+  name: string;
+};
+// User型はHasNameの部分型であるという宣言
+class User implements HasName {
   name: string;
   #age: number;
   constructor(name: string, age: number) {
@@ -12,58 +18,15 @@ class User {
     return this.#age >= 20;
   }
 }
-class PremiumUser extends User {
-  public isAdult(): boolean {
-    return this.#age >= 10; //「#age」はUserに属するプライベートプロパティのため、PremiumUserからはアクセスできない。
-  }
-}
 
-// こう書き換える-------------------------------------------------------------------------
-class User1 {
-  name: string;
-  protected age: number; //「#」をprotectedにすることで子クラスからはアクセスできるようになる
+// nameプロパティを消すとコンパイルエラー 部分型じゃなくなるから
+class User1 implements HasName {
+  #age: number;
   constructor(name: string, age: number) {
     this.name = name;
-    this.age = age;
+    this.#age = age;
   }
   public isAdult(): boolean {
-    return this.age >= 20;
+    return this.#age >= 20;
   }
 }
-class PremiumUser1 extends User1 {
-  public isAdult(): boolean {
-    return this.age >= 10;
-  }
-}
-// ---------------------------------------------------------------------------------------
-const taro = new PremiumUser1("Taro", 15);
-const john = new User1("John", 15);
-console.log(taro.isAdult()); //true
-console.log(john.isAdult()); //false
-
-console.log(taro.age); //コンパイルエラー
-
-// 安易なprotectedの使用はプログラムの複雑化・メンテナンス性の低下につながる。
-class User2 {
-  name: string;
-  protected age: number;
-  private _isAdult: boolean;
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-    this._isAdult = age >= 20;
-  }
-  public isAdult(): boolean {
-    return this._isAdult;
-  }
-}
-class PremiumUser2 extends User2 {
-  public setAge(newAge: number) {
-    this.age = newAge;
-  }
-}
-const taro1 = new PremiumUser2("Taro", 26);
-console.log(taro1.isAdult()); //true
-
-taro1.setAge(10);
-console.log(taro1.isAdult()); //true ageが書き換えられても_isAdultを計算し直さないから結果は変わらない。
