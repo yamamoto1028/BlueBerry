0f1e429d875748f70ef35ec0f04df26aa88a0b29
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-12-02 14:16:05 +0900

    ⚡️ provateとprotectedの動作と使いどころ

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-// override修飾子とその威力
-// override修飾子はあくまでもオーバーライドを明示・宣言するためのものであり、間違っていたらコンパイルエラーを出してもらうためのもの
-// noImplicitOverrideコンパイラオプションを有効化するとオーバーライドするものにはoverride修飾子が必須になる(tsconfig.jsonで設定)
+// privateとprotectedの動作と使いどころ
+// protected → 子クラスからはアクセスできる
+// private(#) → 子クラスからもアクセスできない
 class User {
   name: string;
   #age: number;
@@ -12,11 +12,58 @@ class User {
     return this.#age >= 20;
   }
 }
-
 class PremiumUser extends User {
-  override rank: number = 1; //オーバーライドじゃないものにつけるとコンパイルエラー
-  //オーバーライド↓
-  public override isAdult(): boolean {
-    return true;
+  public isAdult(): boolean {
+    return this.#age >= 10; //「#age」はUserに属するプライベートプロパティのため、PremiumUserからはアクセスできない。
   }
 }
+
+// こう書き換える-------------------------------------------------------------------------
+class User1 {
+  name: string;
+  protected age: number; //「#」をprotectedにすることで子クラスからはアクセスできるようになる
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+  public isAdult(): boolean {
+    return this.age >= 20;
+  }
+}
+class PremiumUser1 extends User1 {
+  public isAdult(): boolean {
+    return this.age >= 10;
+  }
+}
+// ---------------------------------------------------------------------------------------
+const taro = new PremiumUser1("Taro", 15);
+const john = new User1("John", 15);
+console.log(taro.isAdult()); //true
+console.log(john.isAdult()); //false
+
+console.log(taro.age); //コンパイルエラー
+
+// 安易なprotectedの使用はプログラムの複雑化・メンテナンス性の低下につながる。
+class User2 {
+  name: string;
+  protected age: number;
+  private _isAdult: boolean;
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+    this._isAdult = age >= 20;
+  }
+  public isAdult(): boolean {
+    return this._isAdult;
+  }
+}
+class PremiumUser2 extends User2 {
+  public setAge(newAge: number) {
+    this.age = newAge;
+  }
+}
+const taro1 = new PremiumUser2("Taro", 26);
+console.log(taro1.isAdult()); //true
+
+taro1.setAge(10);
+console.log(taro1.isAdult()); //true ageが書き換えられても_isAdultを計算し直さないから結果は変わらない。
