facfb32325335491e3bf372d0bfb0f338d17e9f7
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-12-02 11:28:18 +0900

    ⚡️ (2)親の機能を上書きする

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-// クラスの継承_(1)子は親の機能を受け継ぐ
-// 親クラスを継承して作られた子クラスは親クラスのインスタンスの代わりに使用できる
-// 継承を使うとプログラムの設計が複雑になる傾向がある
-// TypeScriptはクラスがなくてもなんとかなる言語だし、あえてクラスを、しかも継承という複雑なものまで使うかは慎重な判断が必要！
-
-// class クラス名 extends 親クラス {…}
-// class extends 親クラス
+// (2)親の機能を上書き(オーバーライド)する
 
 class User {
   name: string;
@@ -20,17 +14,32 @@ class User {
 
 class PremiumUser extends User {
   rank: number = 1;
+  //ここでisAdultを再宣言
+  public isAdult(): boolean {
+    return true;
+  }
 }
-const taro = new PremiumUser("Taro", 26);
-console.log(taro.rank); //1
-console.log(taro.name); //Taro
-console.log(taro.isAdult()); //true
-console.log(taro);
+const john = new User("John Smith", 17);
+const jiro = new PremiumUser("Jiro", 15);
+console.log(john.isAdult()); //false
+console.log(jiro.isAdult()); //true
 
-//PremiumUser型はUser型の部分型なのでUser型が必要なところにも使える
-function getMassage(u: User) {
-  return `こんにちは${u.name}さん`;
+class PremiumUserNG extends User {
+  rank: number = 1;
+  //ここでisAdultの返り値をstringにしちゃだめ。Userの部分型ということは守らなければいけない
+  public isAdult(): string {
+    return "おとなです";
+  }
 }
-const john = new User("John Smith", 15);
-console.log(getMassage(taro)); //PremiumUser型
-console.log(getMassage(john)); //User型
+
+// コンストラクタもオーバーライドできる
+class PremiumUser1 extends User {
+  rank: number;
+  constructor(name: string, age: number, rank: number) {
+    super(name, age); //super呼び出しを含める必要がある（親クラスのコンストラクタを呼び出す）
+    this.rank = rank;
+  }
+}
+const taro = new PremiumUser1("Taro", 26, 3);
+console.log(taro.name); //Taro
+console.log(taro.rank); //3
